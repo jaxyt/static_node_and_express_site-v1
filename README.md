ac837f39df09527eac250bb15202186eee43f870
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-07-02 18:23:44 -0400

    stylized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # static_node_and_express_site-v1
 Treehouse Express Portfolio Project
+
+I wanted to give it an old school hacker feel.
+The Background color and the Text color have been changed to black and green.
+The Font has been changed to monospace.
--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const app = express();
 const { projects } = require('./data.json');
 const port = 3000;
 
+/**Lets express know to render the web pages using the pug templates in the default views folder */
 app.set("view engine", "pug");
 
+/**Serves all the static images/css/js files when the static route is invoked */
 app.use("/static", express.static("./public"));
 
-app.use(express.static('./images'))
-
+/**rendens the index pug template */
 app.get("/", (req, res)=>{
     res.locals.projects = projects;
     res.render("index");
 });
 
+/**
+ * When the projects route is called, express validates the projects id, and if it exists it renders the
+ * corresponding page, otherwise it throws an error
+ */
 app.get("/projects/:id", (req, res, next)=>{
     if (projects[req.params.id]) {
         res.locals.project = projects[req.params.id];
@@ -26,22 +31,28 @@ app.get("/projects/:id", (req, res, next)=>{
     }
 });
 
+/**renders the about page */
 app.get("/about", (req, res)=>{
     res.render("about");
 });
 
+/**
+ * handles any other routing errors
+ */
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-  
+
+/** renders the error page from the error.pug template */
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
 });
 
+/** starts up the local server */
 app.listen(port, ()=>{
     console.log(`The application is running on port ${port}`);
 });
